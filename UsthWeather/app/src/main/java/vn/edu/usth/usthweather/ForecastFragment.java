c8dd1efb3bf3817de0c9d7be69743448392001d9
La Duy Anh <anhld.22bi13016@usth.edu.vn>
2024-08-30 15:40:57 +0700

    Labwork

--- UsthWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
+++ UsthWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,7 +63,20 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0x20FF0000);
-        return view;
+        view.setBackgroundColor(0x2000FF00);
+
+
+        LinearLayout weather = new LinearLayout(getContext());
+        weather.setOrientation(LinearLayout.VERTICAL);
+        TextView t1 = new TextView(getContext());
+        t1.setText("Thursday");
+        ImageView i1 = new ImageView(getContext());
+        i1.setImageResource(R.drawable.weather_123);
+
+        weather.addView(t1);
+        weather.addView(i1);
+
+        return weather;
+
     }
 }
