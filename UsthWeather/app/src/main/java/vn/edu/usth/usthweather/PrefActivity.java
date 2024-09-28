d992246f43841401660f55151bc82c2ea26284f7
La Duy Anh <anhld.22bi13016@usth.edu.vn>
2024-09-28 11:25:40 +0700

    Labwork 13,14

---
+++ UsthWeather/app/src/main/java/vn/edu/usth/usthweather/PrefActivity.java
@@ -0,0 +1,12 @@
+package vn.edu.usth.usthweather;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrefActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.pref_activity);
+
+    }
+}
--- UsthWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ UsthWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,9 +1,24 @@
 package vn.edu.usth.usthweather;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import android.media.MediaPlayer;
+import androidx.appcompat.widget.Toolbar;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -11,56 +26,35 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.IOException;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.tabs.TabLayout;
-
-import android.media.MediaPlayer;
-
-import android.widget.ImageButton;
-import android.widget.Toast;
-
-
-
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "Weather Activity";
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Log.i(TAG, "onCreate: ");
-        EdgeToEdge.enable(this);
-        View main = findViewById(R.id.main);
 
-        ImageButton button1 = findViewById(R.id.button1);
-        ImageButton button2 = findViewById(R.id.button2);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-             @Override
-            public void onClick(View view) {
-                Toast.makeText(WeatherActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(WeatherActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
 
         ViewPager viewPager = findViewById(R.id.viewpager);
         PagerAdapter adapter = new Adapter(getSupportFragmentManager());
         viewPager.setOffscreenPageLimit(3);
         viewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(viewPager);
 
+        // Copy audio file to internal storage
+        copyAudioFile();
+
+        // Initialize and start MediaPlayer
+        initializeMediaPlayer();
+    }
+
+    private void copyAudioFile() {
         try (InputStream inputStream = getResources().openRawResource(R.raw.audio);
              OutputStream outputStream = new FileOutputStream(new File(getFilesDir(), "audio.mp3"))) {
             byte[] buffer = new byte[1024];
@@ -71,8 +65,10 @@ public class WeatherActivity extends AppCompatActivity {
         } catch (IOException e) {
             Log.e(TAG, "Error copying media file", e);
         }
+    }
 
-        MediaPlayer mediaPlayer = new MediaPlayer();
+    private void initializeMediaPlayer() {
+        mediaPlayer = new MediaPlayer();
         try {
             mediaPlayer.setDataSource(new File(getFilesDir(), "audio.mp3").getPath());
             mediaPlayer.prepare();
@@ -81,7 +77,67 @@ public class WeatherActivity extends AppCompatActivity {
             Log.e(TAG, "Error playing media file", e);
         }
     }
+
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.setting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.pref_activity) {
+            Intent intent = new Intent(WeatherActivity.this, PrefActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.refresh) {
+            // Use AsyncTask to simulate network request
+            new RefreshData().execute("Simulating network refresh...");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class RefreshData extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(WeatherActivity.this, "Refreshing...", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                for (int i = 1; i <= 5; i++) {
+                    Thread.sleep(1000); // Simulate work (1 second for each task)
+                    publishProgress(i * 20); // Simulate progress (20%, 40%, 60%, ...)
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Return a result to onPostExecute
+            return "Data refreshed successfully!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            Toast.makeText(WeatherActivity.this, "Progress: " + values[0] + "%", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(WeatherActivity.this, result, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+    /*@Override
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart: ");
@@ -109,6 +165,5 @@ public class WeatherActivity extends AppCompatActivity {
     protected  void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy: ");
-    }
-
+    }*/
 }
